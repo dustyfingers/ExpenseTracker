244cae132e0aab1764065c70e355a42e796f6e6e
Louie <dustyfingers01257@gmail.com>
2024-12-16 18:18:12 -0700

    build out expense overview component

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+
+import ExpensesSummary from "./ExpensesSummary";
+import ExpensesList from "./ExpensesList";
+
+const ExpensesOutput = ({ expenses, periodName }) => {
+  return (
+    <View>
+      <ExpensesSummary expenses={expenses} periodName={periodName} />
+      <ExpensesList />
+    </View>
+  );
+};
+
+export default ExpensesOutput;
