e4367287ba2e1e3adf904a9edec21b124bde6f98
Louie <dustyfingers01257@gmail.com>
2024-12-23 09:44:21 -0700

    style application a bit

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,7 +1,8 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../constants/styles";
 
 const DUMMY_EXPENSES = [
   {
@@ -38,11 +39,19 @@ const DUMMY_EXPENSES = [
 
 const ExpensesOutput = ({ expenses, periodName }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={periodName} />
       <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+    flex: 1,
+  },
+});
 
 export default ExpensesOutput;
