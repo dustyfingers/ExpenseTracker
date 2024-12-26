df9593f1b379bb608a4d836dea38842600966af4
Louie <dustyfingers01257@gmail.com>
2024-12-26 14:48:52 -0700

    build out manage expense screen a bit more

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,11 +1,67 @@
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { useLayoutEffect } from "react";
 
-const ManageExpense = () => {
+import IconButton from "../components/ui/IconButton";
+import { GlobalStyles } from "../constants/styles";
+import Button from "../components/ui/Button";
+
+const ManageExpense = ({ route, navigation }) => {
+  const id = route.params?.expenseId;
+  const isEditing = !!id;
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+  const deleteExpenseHandler = () => {};
+  const cancelHandler = () => {};
+  const confirmHandler = () => {};
   return (
-    <View>
-      <Text>Manage Expense Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.buttonsContainer}>
+        <Button mode="flat" onPress={cancelHandler} style={styles.button}>
+          Cancel
+        </Button>
+        <Button onPress={confirmHandler} style={styles.button}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            name="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
 
 export default ManageExpense;
