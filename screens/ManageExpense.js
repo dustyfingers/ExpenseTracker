24e28b29046458ee5b59978e20dd97bc22bdc2f7
Louie <dustyfingers01257@gmail.com>
2025-01-05 16:16:53 -0700

    build loading overlay and handle api request errors

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,13 +1,17 @@
 import { View, StyleSheet } from "react-native";
-import { useContext, useLayoutEffect } from "react";
+import { useContext, useLayoutEffect, useState } from "react";
 
 import IconButton from "../components/ui/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import { ExpensesContext } from "../store/expenses-context";
 import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 import { deleteExpense, storeExpense, updateExpense } from "../utils/http";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import ErrorOverlay from "../components/ui/ErrorOverlay";
 
 const ManageExpense = ({ route, navigation }) => {
+  const [submitting, setSubmitting] = useState(false);
+  const [error, setError] = useState();
   const expensesContext = useContext(ExpensesContext);
   const id = route.params?.expenseId;
   const isEditing = !!id;
@@ -20,23 +24,39 @@ const ManageExpense = ({ route, navigation }) => {
     });
   }, [navigation, isEditing]);
   const deleteExpenseHandler = async () => {
-    expensesContext.deleteExpense(id);
-    await deleteExpense(id);
+    setSubmitting(true);
+    try {
+      expensesContext.deleteExpense(id);
+      await deleteExpense(id);
+    } catch (err) {
+      setError("Could not delete expense. Please try again.");
+      setSubmitting(false);
+    }
     navigation.goBack();
   };
   const cancelHandler = () => {
     navigation.goBack();
   };
   const confirmHandler = async (expenseData) => {
-    if (isEditing) {
-      expensesContext.updateExpense(id, expenseData);
-      await updateExpense(id, expenseData);
-    } else {
-      const id = await storeExpense(expenseData);
-      expensesContext.addExpense({ ...expenseData, id });
+    setSubmitting(true);
+    try {
+      if (isEditing) {
+        expensesContext.updateExpense(id, expenseData);
+        await updateExpense(id, expenseData);
+      } else {
+        const id = await storeExpense(expenseData);
+        expensesContext.addExpense({ ...expenseData, id });
+      }
+    } catch (err) {
+      setError("Could not save data. Please try again.");
+      setSubmitting(false);
     }
     navigation.goBack();
   };
+  const errorHandler = () => setError(null);
+  if (error && !submitting)
+    return <ErrorOverlay message={error} onConfirm={errorHandler} />;
+  if (submitting) return <LoadingOverlay />;
   return (
     <View style={styles.container}>
       <ExpenseForm
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -4,8 +4,12 @@ import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../utils/date";
 import { fetchExpenses } from "../utils/http";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import ErrorOverlay from "../components/ui/ErrorOverlay";
 
 const RecentExpenses = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
   const expensesContext = useContext(ExpensesContext);
   const recentExpenses = expensesContext.expenses.filter((expense) => {
     const today = new Date();
@@ -13,12 +17,24 @@ const RecentExpenses = () => {
     return expense.date > date7DaysAgo;
   });
   const getExpenses = async () => {
-    const expenses = await fetchExpenses();
-    expensesContext.setExpenses(expenses);
+    setLoading(true);
+    try {
+      const expenses = await fetchExpenses();
+      expensesContext.setExpenses(expenses);
+    } catch (err) {
+      setError("Could not fetch expenses!");
+    }
+    setLoading(false);
   };
   useEffect(() => {
     getExpenses();
   }, []);
+  const errorHandler = () => {
+    setError(false);
+  };
+  if (error && !loading)
+    return <ErrorOverlay message={error} onConfirm={errorHandler} />;
+  if (loading) return <LoadingOverlay />;
   return (
     <ExpensesOutput
       periodName={"Last 7 Days"}
